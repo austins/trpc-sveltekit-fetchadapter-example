01dedc2f3c4f77da7ba6e190a3a11216907315ec
austins <austins@users.noreply.github.com>
2022-10-18 20:32:02 -0700

    Set port for preview

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 
+const port = 3000;
+
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit()],
     // The port isn't constant by default, so we set the port to 3000 for tRPC server-side fetches.
-    server: { port: 3000 },
+    server: { port },
+    preview: { port },
 };
 
 export default config;
